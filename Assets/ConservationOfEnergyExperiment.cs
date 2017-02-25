240927acf917a1c7120067b11004d18507326f70
marcelo <marcelo>
2017-02-25 04:27:46 -0300

    initial commit: basic structure without sounds

---
+++ Assets/ConservationOfEnergyExperiment.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConservationOfEnergyExperiment : MonoBehaviour {
+
+	private float firstVerticalSpeed = -1f;
+
+	void OnCollisionEnter(Collision collision) {
+		if (this.firstVerticalSpeed < 0) {
+			this.firstVerticalSpeed = collision.relativeVelocity.y;
+		}
+		this.transform.GetComponent<Rigidbody> ().velocity = new Vector3(0, this.firstVerticalSpeed, 0);
+	}
+
+}
